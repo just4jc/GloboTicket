02e64c1de01178812ca180b17b05773b25c022ea
George Ng <just4jcgeorge@gmail.com>
2021-09-20 16:45:36 +0800

    Adding Home.js

--- Home.js
+++ Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { View, Image } from 'react-native';
 
 const Home = (props.) => {
-    return ();
+    return (
+        <View style={{flexDirection: 'column'}}>
+            <Image
+            style={{styles.globologo}}
+            source={require('./images/_Export_globoticket-bug-black.png')}
+        />
+        </View>
+    );
 }
 
 export default Home;
